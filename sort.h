0a833c34363f85f888bfeea00d5ae110214410ad
clementgibot25 <10500@holbertonstudents.com>
2025-04-10 11:52:58 +0200

    fix prototypes in header

--- sort.h
+++ sort.h
@@ -19,10 +19,10 @@ typedef struct listint_s
 } listint_t;
 
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void bubble_sort(int arr[], int n);
-void selection_sort(int arr[], int n);
-void insertion_sort(int arr[], int n);
-void quick_sort(int arr[], int low, int high);
+void print_list(const listint_t **list);
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
